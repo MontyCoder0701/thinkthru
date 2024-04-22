f4e3cd5393922f7eb113c362ff0ca8a65da8c7e8
Soo <soojlee0106@naver.com>
2024-04-22 23:24:02 +0900

    feat: 순서를 가지도록 rule 모델 수정

--- lib/models/rule.dart
+++ lib/models/rule.dart
@@ -1,7 +1,11 @@
 import 'base.dart';
 
 class Rule extends Base {
+  final int order;
   final String content;
 
-  Rule({required this.content});
+  Rule({
+    this.order = 0,
+    required this.content,
+  });
 }
